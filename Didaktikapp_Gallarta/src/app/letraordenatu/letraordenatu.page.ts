eb858aec1e9e0020d10b39d62421c0b522a0faa2
AlvaroViguera18 <78201097+AlvaroViguera18@users.noreply.github.com>
2022-01-26 13:31:57 +0100

    letrak ordenatu

--- Didaktikapp_Gallarta/src/app/letraordenatu/letraordenatu.page.ts
+++ Didaktikapp_Gallarta/src/app/letraordenatu/letraordenatu.page.ts
@@ -21,6 +21,8 @@ export class LetraordenatuPage implements OnInit {
   public desorden: string;
 
   public indexGalderak: number;
+  public indexErantzuna: number;
+
   // public desor: string;
   // public arga: string;
   // public era: string;
@@ -33,6 +35,7 @@ export class LetraordenatuPage implements OnInit {
     this.argaz = this.galderakarray[0].argazkia;
     this.desorden = this.galderakarray[0].desordenatuta;
     this.indexGalderak = 0;
+    this.indexErantzuna = 0;
     this.BotonesDesordenados();
   }
   galderakarray: Galdera[] = [{
@@ -59,55 +62,75 @@ export class LetraordenatuPage implements OnInit {
 
   BotonesDesordenados(){
     for (let i = 0; i < this.galderakarray[this.indexGalderak].desordenatuta.length; i++) {
-      document.getElementById("buttons").innerHTML += "<ion-button style='margin-left: auto; margin-right: auto; display: block; width: 20px; height: 20px'>"+this.galderakarray[this.indexGalderak].desordenatuta.charAt(i)+"</ion-button>";
+      document.getElementById("buttons").innerHTML += "<ion-button id='" + i + "' style='margin-left: auto; margin-right: auto; display: block; width: 20px; height: 20px'>"+this.galderakarray[this.indexGalderak].desordenatuta.charAt(i)+"</ion-button>";
     }
+    for (let i = 0; i < this.galderakarray[this.indexGalderak].desordenatuta.length; i++) {
+      document.getElementById(i.toString()).addEventListener("click", () => {
+        this.addBoton(this.galderakarray[this.indexGalderak].desordenatuta.charAt(i), i);
+        document.getElementById(i.toString()).hidden = true;
+      });
+    }
+
   }
+
   BorrarBotones(){
     document.getElementById("buttons").innerHTML= "";
+    document.getElementById("buttonsOrdenados").innerHTML= "";
+  }
+
+  addBoton(letra, indexBoton){
+    let buttonId = "e" + this.indexErantzuna.toString();
+
+    // eslint-disable-next-line max-len
+    document.getElementById("buttonsOrdenados").innerHTML+= "<ion-button indexPadre='"+indexBoton+"' class='botonErantzuna' id='" + buttonId + "' color='primary' style='margin-left: auto; margin-right: auto; display: block; width: 20px; height: 20px'>" + letra + "</ion-button>";
+
+    let botoiKopurua = document.getElementsByClassName('botonErantzuna');
+
+    for(let i=0;i<botoiKopurua.length;i++){
+      let idBoton = botoiKopurua[i].id;
+      let idPadre = botoiKopurua[i].getAttribute("indexPadre").toString();
+
+      document.getElementById(idBoton).addEventListener("click", () => {
+        document.getElementById(idPadre).hidden = false;
+        document.getElementById(idBoton).remove();
+      });
+    }
+
+    this.indexErantzuna = this.indexErantzuna + 1;
   }
 
   hurrengoHitza(){
-    this.indexGalderak = this.indexGalderak + 1;
-    this.gald = this.galderakarray[this.indexGalderak].galdera;
-    this.argaz = this.galderakarray[this.indexGalderak].argazkia;
-    if (this.indexGalderak < this.galderakarray.length) {
-      this.BorrarBotones();
-      this.BotonesDesordenados();
-    }else {
-      alert("Hemos llegado al final");
+    const erantzunZuzena = this.galderakarray[this.indexGalderak].erantzuna;
+    let erabiltzaileErantzuna = "";
+
+    for (let i = 0; i < this.indexErantzuna; i++){
+      erabiltzaileErantzuna += document.getElementById('e' + i).innerHTML.toString();
     }
+
+    if(erantzunZuzena === erabiltzaileErantzuna){
+      this.indexGalderak = this.indexGalderak + 1;
+      this.gald = this.galderakarray[this.indexGalderak].galdera;
+      this.argaz = this.galderakarray[this.indexGalderak].argazkia;
+      if (this.indexGalderak < this.galderakarray.length) {
+        this.BorrarBotones();
+        this.indexErantzuna = 0;
+        this.BotonesDesordenados();
+      }else {
+        alert("Hemos llegado al final");
+      }
+    }
+    else{
+      for(let i = 0;i<erabiltzaileErantzuna.length;i++){
+        if(erabiltzaileErantzuna.charAt(i) !== erantzunZuzena.charAt(i)){
+          document.getElementById('e' + i).setAttribute("color", "danger");
+        }
+      }
+    }
+
+
     // this.indexGalderak = this.indexGalderak + 1;
     // this.BotonesDesordenados();
 
   }
 
-  // hurrengoGaldera() {
-  //   this.indexGalderak = this.indexGalderak + 1;
-  //   if (this.indexGalderak < this.galderakarray.length) {
-  //     if (this.galderakarray[this.indexGalderak].erantzuna3 === '') {
-  //       this.era3Visible = false;
-  //     } else {
-  //       this.era3Visible = true;
-  //     }
-  //     if (this.galderakarray[this.indexGalderak].erantzuna4 === '') {
-  //       this.era4Visible = false;
-  //     } else {
-  //       this.era4Visible = true;
-  //     }
-  //
-  //     this.gald = this.galderakarray[this.indexGalderak].galdera;
-  //     this.era = this.galderakarray[this.indexGalderak].erantzuna;
-  //     this.era2 = this.galderakarray[this.indexGalderak].erantzuna2;
-  //     this.era3 = this.galderakarray[this.indexGalderak].erantzuna3;
-  //     this.era4 = this.galderakarray[this.indexGalderak].erantzuna4;
-  //     this.aukeratutakoerantzuna='0';
-  //   }
-  //   else{
-  //     this.indexGalderak = this.indexGalderak - 1;
-  //
-  //     this.abrirAlert();
-  //
-  //     this.controller.dismiss();
-  //   }
-  // }
 }
